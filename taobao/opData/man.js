e79eb26f919f7ecb242212e3fe59f192bcc9ac73
guossnh <guossnh@gmail.com>
2019-08-01 17:13:32 +0800

    add

--- taobao/opData/man.js
+++ taobao/opData/man.js
@@ -9,7 +9,8 @@ var yestday = new Date(new Date().getTime() - (1000 * 60 * 60 * 24))
 var beforeYestay = new Date(new Date().getTime() - (1000 * 60 * 60 * 48))
 var yestdayAllData = [];
 var beforeYestdayAllData = [];
-var getOperationDataLink = "http://qiancaotang.oicp.vip/magicflu/service/s/jsonv2/d553a687-4234-4536-9fe5-8489c8dfacc3/forms/c323773c-8db0-477d-84d2-c7d5cd17ee5c/records/entry?limit=-1&start=0"
+var getOperationDataLink = "http://qiancaotang.oicp.vip/magicflu/service/s/jsonv2/d553a687-4234-4536-9fe5-8489c8dfacc3/forms/c323773c-8db0-477d-84d2-c7d5cd17ee5c/records/entry?limit=-1&start=0&bq="
+http://qiancaotang.oicp.vip/magicflu/service/s/jsonv2/d553a687-4234-4536-9fe5-8489c8dfacc3/forms/c323773c-8db0-477d-84d2-c7d5cd17ee5c/records/entry?limit=-1&start=0&bq=riqi(eq):2019-07-31
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~我是华丽的分割线~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
 需要的方法
@@ -19,7 +20,6 @@ var getOperationDataLink = "http://qiancaotang.oicp.vip/magicflu/service/s/jsonv
 $(function () {
 
 });
-
 
 //这个 方法是判断两个date格式的年月日是相等的
 function dateSame(date1, date2) {
@@ -33,8 +33,9 @@ function dateSame(date1, date2) {
 //通过日期获取运营数据表格的数据并且返回json格式
 function get_data_by_date(date) {
   var result_data = "";
+  //对日期做判断需要加0
   $.ajax({
-    url: getOperationDataLink + "&bq=" + date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate() + "%26%26updated(orderby)%3Adesc",
+    url: getOperationDataLink + "riqi(eq):" +return_date_url_use(date),
     data: {},
     dataType: 'json',
     async: false,
@@ -44,6 +45,19 @@ function get_data_by_date(date) {
   });
   return result_data;
 };
+
+//传入日期返回一个链接使用的日期字符串
+function return_date_url_use(date){
+  //这个方法判断月份和日期是否是一位如果是的话需要前边加0
+  function get_date_str(nu){
+    if(String(nu).length==1){
+      return "0"+String(nu);
+    }else{
+      return nu;
+    }
+  }
+  return date.getFullYear()+"-"+get_date_str(date.getMonth())+"-"+get_date_str(date.getDate())
+}
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~我是华丽的分割线~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
 man
