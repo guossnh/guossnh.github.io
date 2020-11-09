1515a8ade45703c297085a5844b28c49dcf07521
guossnh <guossnh@gmail.com>
2020-11-09 17:33:16 +0800

    add

--- taobao/dtb_get_data/userscript.js
+++ taobao/dtb_get_data/userscript.js
@@ -8,42 +8,41 @@
 // @downloadURL http://guossnh.github.io/dtb_get_data/userscript.js
 // @include    https://www.duotuiba.com/*
 // @require  http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js
+// @connect *
 // ==/UserScript==
 
-
-
-
-function get_dtb(page_id){
-    //var jsondata = {orderSn: product_id,remark: user_remark,source: 1};
-    var get_time = new Date().getTime();
-    $.ajax({
-        url: "https://web.duotuiba.com/v1/web/order/list?page="+page_id+"&platfrom=0&task_id=0&goods_id=0&state=-1&p_order_id=0",
-        method: "GET",
-        Accept: "*/*",
-        'Accept-Encoding': "gzip, deflate, br",
-        'Accept-Language': "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        Connection: "keep-alive",
-        Host: "web.duotuiba.com",
-        Origin: "https://www.duotuiba.com",
-        platform: web,
-        Referer: "https://www.duotuiba.com/",
-        'Sec-Fetch-Dest': empty,
-        'Sec-Fetch-Mode': cors,
-        'Sec-Fetch-Site': "same-site",
-        timestamp: get_time,
-        token: "2oU29yfypPCQV1dO48Nxb7eZitoaH6oK",
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36",
-        success: function (data) {
-            console.log("成功返回数据");
-            console.log(data.success);
-            console.log(data);
-        }
-      });
+function click_button(page_num){
+    setTimeout(function(){
+        var order_id,product_id,money,time,state;
+        document.getElementsByClassName("ant-pagination-item ant-pagination-item-"+page_num+"")[0].click();
+        var one = document.getElementById("rongqi");
+        var table = document.getElementsByClassName("ant-table-tbody")[0].children;
+            for(var i=0;i<table.length;i++){
+                order_id = table[i].children[0].innerHTML
+                product_id = table[i].children[3].innerHTML
+                money = table[i].children[5].children[0].innerText.replace("￥","");
+                state = table[i].children[6].children[0].children[1].innerText;
+                time = table[i].children[7].children[0].children[0].innerText.replace("下单:","");
+                console.log(order_id,product_id,money,time,state)
+                one.append(""+order_id+","+product_id+","+money+","+time+","+state+"")
+            }
+    },2000*page_num);
 }
 
+function creat_div(){
+    var wocao = document.createElement("div");
+    wocao.setAttribute("id","rongqi");
+    document.getElementsByTagName("body")[0].append(wocao);
+}
 
 (function() {
     'use strict';
-
-    get_dtb(1)
+    var $ = window.jQuery;
+    creat_div();
+    var c=0;
+    for(let i=1;i<5;i++){
+            click_button(i);
+    }
+    
+    
 })();
